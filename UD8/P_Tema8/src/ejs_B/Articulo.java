b0d74fd9592118899d002ba941ce82063ba844d7
alexCaste02 <alexcaste2002gs@gmail.com>
2022-12-20 10:07:36 +0100

    Structure Fix

---
+++ UD8/P_Tema8/src/ejs_B/Articulo.java
@@ -0,0 +1,15 @@
+package ejs_B;
+
+/*
+Crea un programa con una clase llamada Articulo con los siguientes atributos: nombre,
+precio (sin IVA), iva (siempre será 21) y cuantosQuedan (representa cuantos quedan en
+el almacén).
+ */
+
+public class Articulo {
+    String nombre;
+    double precio;
+    final int IVA = 21;
+    int cuantosQuedan;
+
+}
