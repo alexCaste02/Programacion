21d9d9d634d00de81cc527f809c91df7537a3b9a
alexCaste02 <alexcaste2002gs@gmail.com>
2022-12-21 10:18:14 +0100

    created new package C

---
+++ UD8/Proyecto_8/src/ejs_C/Articulo.java
@@ -0,0 +1,27 @@
+package ejs_C;
+
+/*
+Crea un programa con una clase llamada Articulo con los siguientes atributos: nombre,
+precio (sin IVA), iva (siempre será 21) y cuantosQuedan (representa cuantos quedan en
+el almacén).
+ */
+
+public class Articulo {
+    String nombre;
+    double precio;
+    int IVA;
+    int cuantosQuedan;
+
+    public Articulo(String nombre, double precio, int IVA, int cuantosQuedan) {
+
+        if (IVA==21 && precio>0 && cuantosQuedan>0){
+            this.nombre = nombre;
+            this.precio = precio;
+            this.IVA = IVA;
+            this.cuantosQuedan = cuantosQuedan;
+        } else {
+            System.err.println("ERROR al instanciar Articulo...");
+        }
+
+    }
+}
---
+++ UD8/Proyecto_8/src/ejs_C/MainArticulo.java
@@ -0,0 +1,28 @@
+package ejs_C;
+
+/*
+En el main de la clase principal instancia un objeto de la clase artículo. Asígnale valores a
+todos sus atributos (los que quieras) y muestra por pantalla un mensaje del estilo
+“Pijama - Precio:10€ - IVA:21% - PVP:12,1€” (el PVP es el precio de venta al público, es
+decir, el precio con IVA). Luego, cambia el precio y vuelve a imprimir el mensaje.
+ */
+
+public class MainArticulo {
+    public static void main(String[] args) {
+
+        Articulo art1 = new Articulo("Cereales",4.3,21,12);
+        Articulo art2 = new Articulo("Arroz",-3,21,12);
+        Articulo art3 = new Articulo("Leche",4.3,19,-1);
+
+        System.out.printf("%s - Precio: %.2f - IVA: %d - PVP: %.2f %n",art1.nombre,art1.precio,art1.IVA,art1.precio+art1.precio*art1.IVA/100);
+
+
+        art1.precio = 2.15;
+
+        System.out.printf("%s - Precio: %.2f - IVA: %d - PVP: %.2f %n",art1.nombre,art1.precio,art1.IVA,art1.precio+art1.precio*art1.IVA/100);
+
+
+    }
+
+
+}
