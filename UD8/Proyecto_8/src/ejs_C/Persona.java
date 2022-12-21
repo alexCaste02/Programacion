a1e351914f21391e2490c8c914d4a94c685473f9
alexCaste02 <alexcaste2002gs@gmail.com>
2022-12-21 10:41:25 +0100

    some work C2

--- UD8/Proyecto_8/src/ejs_C/Persona.java
+++ UD8/Proyecto_8/src/ejs_C/Persona.java
@@ -6,15 +6,47 @@ de una persona: dni, nombre, apellidos y edad.
 */
 
 public class Persona {
-    String dni;
-    String nombre;
-    String apellidos;
-    int edad;
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private int edad;
 
     public Persona(String dni, String nombre, String apellidos, int edad) {
         this.dni = dni;
         this.nombre = nombre;
         this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
         this.edad = edad;
     }
 }
