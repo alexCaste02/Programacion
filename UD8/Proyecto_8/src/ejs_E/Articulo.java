85f230e63c5115950ee26f71363de50f0f0febc7
alexCaste02 <alexcaste2002gs@gmail.com>
2023-01-07 14:47:14 +0100

    almost finished E1.Punto

---
+++ UD8/Proyecto_8/src/ejs_E/Articulo.java
@@ -0,0 +1,97 @@
+package ejs_E;
+
+/*
+Crea un programa con una clase llamada Articulo con los siguientes atributos: nombre,
+precio (sin IVA), iva (siempre será 21) y cuantosQuedan (representa cuantos quedan en
+el almacén).
+ */
+
+public class Articulo {
+    private String nombre;
+    private double precio;
+    private int IVA;
+    private int cuantosQuedan;
+
+    public Articulo(String nombre, double precio, int IVA, int cuantosQuedan) {
+
+        if (IVA==21 && precio>0 && cuantosQuedan>0){
+            this.nombre = nombre;
+            this.precio = precio;
+            this.IVA = IVA;
+            this.cuantosQuedan = cuantosQuedan;
+        } else {
+            System.err.println("ERROR al instanciar Articulo...");
+        }
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getIVA() {
+        return IVA;
+    }
+
+    public void setIVA(int IVA) {
+        this.IVA = IVA;
+    }
+
+    public int getCuantosQuedan() {
+        return cuantosQuedan;
+    }
+
+    public void setCuantosQuedan(int cuantosQuedan) {
+        this.cuantosQuedan = cuantosQuedan;
+    }
+
+    public void imprime(){
+        System.out.printf("""
+                Nombre: %s
+                Precio: %.2f
+                PVP: %.2f
+                IVA: %d
+                Quedan: %d
+                
+                """,nombre,precio,getPVP(),IVA,cuantosQuedan);
+    }
+
+    public double getPVP(){
+        return (precio+precio*IVA/100);
+    }
+
+    public double getPVPDescuento(double descuento){
+        return ( getPVP() * (1-(descuento/100)) );
+        //hace falta comprobar que es de 0-100?
+    }
+
+    public boolean vender(int cantVendida){
+        if (cantVendida>cuantosQuedan || cantVendida<1) {
+            return false;
+        }else{
+            cuantosQuedan-=cantVendida;
+            return true;
+        }
+    }
+
+    public boolean almacenar(int cantAlmacenar){
+        if (cantAlmacenar<1) {
+            return false;
+        }else{
+            cuantosQuedan+=cantAlmacenar;
+            return true;
+        }
+    }
+}
---
+++ UD8/Proyecto_8/src/ejs_E/MainPunto.java
@@ -0,0 +1,46 @@
+package ejs_E;
+
+/*
+En el main de la clase principal instancia 3 objetos Punto con las coordenadas (5,0),
+(10,10) y (-3, 7). Muestra por pantalla sus coordenadas (utiliza un println para cada
+punto). Modifica todas las coordenadas (prueba distintos operadores como = + - +=
+*=...) y vuelve a imprimirlas por pantalla.
+*/
+
+public class MainPunto {
+    public static void main(String[] args) {
+
+//        Punto punto1 = new Punto(5,0);
+//        Punto punto2 = new Punto(10,10);
+//        Punto punto3 = new Punto(3,7);
+//
+//
+//        System.out.println(punto1.getX()+","+punto1.getY());
+//        System.out.println(punto2.getX()+","+punto2.getY());
+//        System.out.println(punto3.getX()+","+punto3.getY());
+//
+//
+//        punto1.setX(punto1.getX()+1);
+//        punto1.setY(punto1.getY()-2);
+//
+//        punto2.setX(punto1.getX()*3);
+//        punto2.setY(punto1.getY()/4);
+//
+//        punto3.setX(punto1.getX()+5);
+//        punto3.setY(punto1.getY()-6);
+//
+//
+//
+//        System.out.println(punto1.getX()+","+punto1.getY());
+//        System.out.println(punto2.getX()+","+punto2.getY());
+//        System.out.println(punto3.getX()+","+punto3.getY());
+//
+//
+//        punto1.imprime();
+//        punto1.setXY(2,5);
+//        punto1.desplaza(1,1);
+//        punto1.imprime();
+//        punto2.imprime();
+//        System.out.println(punto1.distancia(punto2));
+    }
+}
