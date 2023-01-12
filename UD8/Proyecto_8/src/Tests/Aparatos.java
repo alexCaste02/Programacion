745b951f490d948d326f0cb19533977b2976ec02
alexCaste02 <alexcaste2002gs@gmail.com>
2023-01-12 18:31:51 +0100

    stuff

---
+++ UD8/Proyecto_8/src/Tests/Aparatos.java
@@ -0,0 +1,38 @@
+package Tests;
+
+public class Aparatos {
+    //atributos
+    private String nombre;
+    private boolean encendido=false;
+    private int watios;
+    private static int consumoInicial=0;
+
+    //constructor
+    public Aparatos(String nombre, int watios){
+        this.nombre=nombre;
+        this.watios=watios;
+    }
+
+    //metodo
+    public void imprime(){
+        System.out.println();
+    }
+
+    public void encenderYApagar(){
+        encendido=!encendido;
+    }
+
+
+
+
+
+
+
+    public int consumoTotal(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
