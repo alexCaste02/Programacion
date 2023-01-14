4fc4dd1177fd565e9d83d913879e5316910124d5
alexCaste02 <alexcaste2002gs@gmail.com>
2023-01-14 17:26:19 +0100

    fixes?

--- UD8/Proyecto_8/src/ejs_2_7/Main.java
+++ UD8/Proyecto_8/src/ejs_2_7/Main.java
@@ -3,7 +3,8 @@ package ejs_2_7;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("hi");
+        int segundos = 120;
+        System.out.println(2.5*(segundos/60));
 
     }
 }
