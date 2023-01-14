8e4478e6d5e63bb5b5a71079acfbd06036ec02ef
alexCaste02 <alexcaste2002gs@gmail.com>
2023-01-14 20:22:35 +0100

    mostly finished 2.7

--- UD8/Proyecto_8/src/ejs_2_7/Main.java
+++ UD8/Proyecto_8/src/ejs_2_7/Main.java
@@ -3,8 +3,32 @@ package ejs_2_7;
 public class Main {
     public static void main(String[] args) {
 
-        int segundos = 120;
-        System.out.println(2.5*(segundos/60));
+        //F al final del numero para que funcione como un Float
+        MovilPrepago.setCosteConsumoMB(0.2F);
+        MovilPrepago.setCosteMinutoLLamada(0.15F);
+        MovilPrepago.setCosteEstableceLLamada(0.5F);
+
+        //L al final del numero para que funcione como un Long
+        MovilPrepago movil1 = new MovilPrepago(1234567890123L);
+        MovilPrepago movil2 = new MovilPrepago(9999999999999L);
+        MovilPrepago movil3 = new MovilPrepago(999999999999L);
+
+        System.out.println("Saldo: "+movil1.consultarSaldo()+"\n");
+
+        movil1.recargar(5);
+        System.out.println("Saldo: "+movil1.consultarSaldo()+"\n");
+
+        movil1.efectuarLlamada(135);
+        System.out.println("Saldo: "+movil1.consultarSaldo()+"\n");
+
+        movil1.navegar(50);
+        System.out.println("Saldo: "+movil1.consultarSaldo()+"\n");
+
+        movil1.recargar(5);
+        System.out.println("Saldo: "+movil1.consultarSaldo()+"\n");
+
+
+
 
     }
 }
--- UD8/Proyecto_8/src/ejs_2_7/MovilPrepago.java
+++ UD8/Proyecto_8/src/ejs_2_7/MovilPrepago.java
@@ -2,19 +2,117 @@ package ejs_2_7;
 
 public class MovilPrepago {
 
-    private int numeroMovil;
-    private float costeEstableceLLamada;
-    private float costeMinutoLLamada;
-    private float costeCostumoMB;
-    private float saldo;
+    //no podemos usar int para un numero de 13 digitos
+    private final long numeroMovil;
+    private static float costeEstableceLLamada, costeMinutoLLamada, costeConsumoMB;
+    private float saldo=0;
+
+    /* CONSTRUCTORES */
+    public MovilPrepago(long numeroMovil) {
+        if ( (numeroMovil+"").length()==13 ) {
+            this.numeroMovil=numeroMovil;
+        } else {
+            System.err.println("Formato de numero invalido");
+            this.numeroMovil=0;
+        }
+    }
+
+
+    /* GETTERS Y SETTERS */
+
+    public static float getCosteEstableceLLamada() {
+        return costeEstableceLLamada;
+    }
+    public static void setCosteEstableceLLamada(float costeEstableceLLamada) {
+        MovilPrepago.costeEstableceLLamada = costeEstableceLLamada;
+    }
+    public static float getCosteMinutoLLamada() {
+        return costeMinutoLLamada;
+    }
+    public static void setCosteMinutoLLamada(float costeMinutoLLamada) {
+        MovilPrepago.costeMinutoLLamada = costeMinutoLLamada;
+    }
+    public static float getCosteConsumoMB() {
+        return costeConsumoMB;
+    }
+    public static void setCosteConsumoMB(float costeCostumoMB) {
+        MovilPrepago.costeConsumoMB = costeCostumoMB;
+    }
+
+
+    /* OTROS */
 
     public void efectuarLlamada (int segundos){
-        saldo-=costeMinutoLLamada*(segundos/60);
+
+//        saldo-= (costeEstableceLLamada+costeMinutoLLamada*(segundos/60));
+//
+//        if(saldo<0) {
+//            System.out.println("Saldo insuficiente, llamada cortada");
+//            saldo=0;
+//        }
+
+
+        int duracion = segundos;
+        saldo -= costeEstableceLLamada;
+
+        while(segundos > 0){
+
+            saldo-=costeMinutoLLamada;
+
+            if (saldo<=0){
+                System.out.println("Saldo insuficiente, llamada cortada");
+                saldo=0;
+                duracion-=segundos;
+                break;
+            }
+
+            segundos -= 60;
+
+        }
+
+        System.out.println("Duracion de la llamada: "+(duracion/60)+" minutos y "+(duracion%60)+" segundos.");
+
     }
 
-    public void navegar(int MB){
+    public void navegar (int mb){
+
+        //A lo mejor hay una forma mas limpia pero que pereza xd, uso la de llamada.
+
+
+        int mbNavegados = mb;
+
+        while(mb > 0){
+
+            saldo-=costeMinutoLLamada;
+
+            if (saldo<=0){
+                System.out.println("Saldo insuficiente, navegacion cortada");
+                saldo=0;
+                mbNavegados-=mb;
+                break;
+            }
+
+            mb -= 1;
+
+        }
+
+        System.out.println("MB navegados: "+mbNavegados);
 
     }
+
+    public boolean recargar (int importe){
+        if (importe%5==0) {
+            saldo+=importe;
+            return true;
+        }
+        return false;
+    }
+
+    public float consultarSaldo(){
+        return saldo;
+    }
+
+
 
 
 
