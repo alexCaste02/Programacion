baba7ce983c3e7b4edb52a09815a2335bcba8908
alexCaste02 <alexcaste2002gs@gmail.com>
2023-01-16 17:49:40 +0100

    testing alt 2.9

--- UD8/Proyecto_8/src/ejs_2_9_ALT/Main.java
+++ UD8/Proyecto_8/src/ejs_2_9_ALT/Main.java
@@ -1,58 +1,42 @@
-//package ejs_2_9_ALT;
-//
-//import java.util.Scanner;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//
-//        Scanner input = new Scanner(System.in);
-//        boolean adivinado = false;
-//
-//        System.out.println("Introduce la longitud del numero que quieres adivinar");
-//        MasterMind resp1 = new MasterMind(input.nextInt());
-////        System.out.println("## NUMERO GENERADO: " + resp1.getNumeroCorrecto() + " ##");
-//
-//
-//        while (resp1.quedanIntentos() && !adivinado) {
-//            System.out.println(resp1.getIntentos() + " intento(s) restante(s)");
-//            System.out.println("Introduce un numero de " + resp1.getNumeroCorrecto().length() + " digitos");
-//            resp1.setNumIntroducido(input.next());
-//
-//            if(resp1.comprobarRespuesta()) {
-//                adivinado = true;
-//            } else {
-//                System.out.println(resp1.getDigitosPosCorrecta() + " digito(s) bien colocado(s), " + resp1.getDigitosMalColocados() + " digito(s) mal colocado(s)");
-//            }
-//        }
-//
-//        System.out.println(adivinado?"Has acertado":"Te has quedado sin intentos");
-//    }
-//}
-
 package ejs_2_9_ALT;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
-        System.out.println("Introduce la longitud del numero que quieres adivinar:");
-        MasterMind game = new MasterMind(input.nextInt());
-        while (game.getAttemptsLeft() > 0) {
-            System.out.println(game.getAttemptsLeft() + " intento(s) restante(s)");
-            System.out.println("Introduce un numero de " + game.getCorrectNumber().length() + " digitos:");
-            game.setEnteredNumber(input.next());
-            if (game.checkAnswer()) {
-                System.out.println("Has acertado!");
-                break;
-            } else {
-                System.out.println(game.getCorrectlyPlacedDigits() + " digito(s) bien colocado(s), " + game.getIncorrectlyPlacedDigits() + " digito(s) mal colocado(s)");
+
+        boolean hasWon = false;
+        MasterMind game = new MasterMind(4, 10);
+
+        while (game.hasAttemptsLeft()) {
+            System.out.println("You have " + game.getAttemptsLeft() + " attempts left.");
+            System.out.println("Enter a 4-digit number without repeating digits:");
+            String enteredNumber = input.nextLine();
+
+            if (!game.isValidInput(enteredNumber)) {
+                System.out.println("Invalid input, please enter a 4-digit number without repeating digits.");
+                continue;
             }
+            game.setEnteredNumber(enteredNumber);
+
+            if (game.checkAnswer()) {
+                hasWon = true;
+                break;
+            }
+            System.out.println(game.getCorrectlyPlacedDigits() + " digit(s) are correctly placed, " + game.getIncorrectlyPlacedDigits() + " digit(s) are in the correct number but in the wrong position.");
         }
-        if (game.getAttemptsLeft() == 0) {
-            System.out.println("Te has quedado sin intentos. El numero correcto era: " + game.getCorrectNumber());
+
+        if (hasWon) {
+            System.out.println("Congratulations! You have won the game. The correct number was " + game.getCorrectNumber() + ".");
+        } else {
+            System.out.println("Sorry, you have run out of attempts. The correct number was " + game.getCorrectNumber() + ".");
         }
     }
 }
+
+
+
 
 
--- UD8/Proyecto_8/src/ejs_2_9_ALT/MasterMind.java
+++ UD8/Proyecto_8/src/ejs_2_9_ALT/MasterMind.java
@@ -1,145 +1,48 @@
-//package ejs_2_9_ALT;
-//
-//import java.util.concurrent.ThreadLocalRandom;
-//
-//public class MasterMind {
-//
-//    private String numeroCorrecto = "";
-//    private StringBuilder numerosDisponibles = new StringBuilder("0123456789");
-//    private String numIntroducido;
-//    private int digitosPosCorrecta, digitosMalColocados, intentos = 10;
-//
-//
-//    public MasterMind(int longitudNumero) {
-//
-//        int posRand;
-//
-//        for(int i=longitudNumero; i>0; i--){
-//
-//            posRand = ThreadLocalRandom.current().nextInt(0,numerosDisponibles.length());
-//            numeroCorrecto += numerosDisponibles.charAt(posRand);
-//            numerosDisponibles.deleteCharAt(posRand);
-//
-//        }
-//
-//    }
-//
-//
-//    public String getNumeroCorrecto() {
-//        return numeroCorrecto;
-//    }
-//    public void setNumeroCorrecto(String numeroCorrecto) {
-//        this.numeroCorrecto = numeroCorrecto;
-//    }
-//
-//    public StringBuilder getNumerosDisponibles() {
-//        return numerosDisponibles;
-//    }
-//    public void setNumerosDisponibles(StringBuilder numerosDisponibles) {
-//        this.numerosDisponibles = numerosDisponibles;
-//    }
-//
-//    public String getNumIntroducido() {
-//        return numIntroducido;
-//    }
-//    public void setNumIntroducido(String numIntroducido) {
-//        this.numIntroducido = numIntroducido;
-//    }
-//
-//    public int getDigitosPosCorrecta() {
-//        return digitosPosCorrecta;
-//    }
-//    public void setDigitosPosCorrecta(int digitosPosCorrecta) {
-//        this.digitosPosCorrecta = digitosPosCorrecta;
-//    }
-//
-//    public int getDigitosMalColocados() {
-//        return digitosMalColocados;
-//    }
-//    public void setDigitosMalColocados(int digitosMalColocados) {
-//        this.digitosMalColocados = digitosMalColocados;
-//    }
-//
-//    public int getIntentos() {
-//        return intentos;
-//    }
-//    public void setIntentos(int intentos) {
-//        this.intentos = intentos;
-//    }
-//
-//    public boolean comprobarRespuesta() {
-//
-//        digitosMalColocados = 0;
-//        digitosPosCorrecta = 0;
-//        intentos--;
-//
-//        for (int i = 0; i < numeroCorrecto.length(); i++) {
-//
-//            if (estaDentro(numIntroducido.charAt(i))) {
-//
-//                if (posicionCorrecta(numIntroducido.charAt(i))) {
-//                    digitosPosCorrecta++;
-//                } else {
-//                    digitosMalColocados++;
-//                }
-//            }
-//        }
-//
-//        return digitosPosCorrecta == numeroCorrecto.length();
-//
-//    }
-//
-//    public boolean quedanIntentos(){
-//        return intentos > 0;
-//    }
-//
-//    public boolean estaDentro(char c){
-//        return numeroCorrecto.indexOf(c) != -1;
-//    }
-//
-//    public boolean posicionCorrecta(char c){
-//        return numeroCorrecto.indexOf(c) == numIntroducido.indexOf(c);
-//    }
-//
-//}
-
 package ejs_2_9_ALT;
 
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MasterMind {
 
-    private final int totalAttempts = 10;
-    private final int numberLength;
-    private final String correctNumber;
+    private final int numberOfDigits;
+    private final int totalAttempts;
+    private String correctNumber;
     private String enteredNumber;
-    private int attemptsLeft = totalAttempts;
+    private int attemptsLeft;
     private int correctlyPlacedDigits;
     private int incorrectlyPlacedDigits;
 
-    public MasterMind(int numberLength) {
-        this.numberLength = numberLength;
-        StringBuilder availableNumbers = new StringBuilder("0123456789");
-        StringBuilder correctNumberBuilder = new StringBuilder();
-        int posRand;
-        for (int i = 0; i < numberLength; i++) {
-            posRand = ThreadLocalRandom.current().nextInt(0, availableNumbers.length());
-            correctNumberBuilder.append(availableNumbers.charAt(posRand));
-            availableNumbers.deleteCharAt(posRand);
+    public MasterMind(int numberOfDigits, int totalAttempts) {
+        this.numberOfDigits = numberOfDigits;
+        this.totalAttempts = totalAttempts;
+        String digits = "0123456789";
+        correctNumber = "";
+        for (int i = 0; i < numberOfDigits; i++) {
+            int posRand = ThreadLocalRandom.current().nextInt(0, digits.length());
+            correctNumber += digits.charAt(posRand);
+            digits = digits.substring(0, posRand) + digits.substring(posRand+1);
         }
-        correctNumber = correctNumberBuilder.toString();
+        attemptsLeft = totalAttempts;
+    }
+
+    public int getNumberOfDigits() {
+        return numberOfDigits;
+    }
+
+    public int getTotalAttempts() {
+        return totalAttempts;
     }
 
     public String getCorrectNumber() {
         return correctNumber;
     }
 
-    public String getEnteredNumber() {
-        return enteredNumber;
-    }
-
     public void setEnteredNumber(String enteredNumber) {
         this.enteredNumber = enteredNumber;
+    }
+
+    public String getEnteredNumber() {
+        return enteredNumber;
     }
 
     public int getAttemptsLeft() {
@@ -154,14 +57,42 @@ public class MasterMind {
         return incorrectlyPlacedDigits;
     }
 
-    public boolean checkAnswer() {
-        if (enteredNumber.length() != numberLength) {
-            throw new IllegalArgumentException("Entered number should have " + numberLength + " digits");
-        }
-        if (attemptsLeft == 0) {
+    public boolean isValidInput(String input) {
+        if (input.length() != numberOfDigits) {
             return false;
         }
+        for (int i = 0; i < numberOfDigits; i++) {
+            for (int j = i+1; j < numberOfDigits; j++) {
+                if (input.charAt(i) == input.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean checkAnswer() {
         attemptsLeft--;
-        correctlyPlacedDigits =
+        correctlyPlacedDigits = 0;
+        incorrectlyPlacedDigits = 0;
+        for (int i = 0; i < numberOfDigits; i++) {
+            if (correctNumber.indexOf(enteredNumber.charAt(i)) != -1) {
+                if (enteredNumber.charAt(i) == correctNumber.charAt(i)) {
+                    correctlyPlacedDigits++;
+                } else {
+                    incorrectlyPlacedDigits++;
+                }
+            }
+        }
+        return correctlyPlacedDigits == numberOfDigits;
+    }
+
+    public boolean hasAttemptsLeft() {
+        return attemptsLeft > 0;
+    }
+}
+
+
+
 
 
