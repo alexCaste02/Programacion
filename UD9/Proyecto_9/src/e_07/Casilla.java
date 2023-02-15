d3ff4572a533659ba8bd8ecce99210771306179b
Alex <alexcaste2002gs@gmail.com>
2023-02-15 11:55:28 +0100

    Update Casilla.java

--- UD9/Proyecto_9/src/e_07/Casilla.java
+++ UD9/Proyecto_9/src/e_07/Casilla.java
@@ -2,5 +2,17 @@ package e_07;
 
 public class Casilla {
     private final String nombre;
+<<<<<<< Updated upstream
     int codigo;
+=======
+    private final int codigo;
+
+    public Casilla(String nombre, int codigo) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+    }
+    
+
+
+>>>>>>> Stashed changes
 }
