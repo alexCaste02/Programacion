28eb894fb7baddfdbf3a32c7748319e103744d4a
alexCaste02 <alexcaste2002gs@gmail.com>
2023-03-21 08:55:03 +0100

    cosas

---
+++ EXPERIMENTS/src/test.java
@@ -0,0 +1,13 @@
+public class test {
+    public static void main(String[] args) {
+        String[] test = {"tonto!!!!"};
+
+        for (String i : test) {
+            System.out.println(i);
+        }
+        /*
+        System.out.println("hola");
+        */
+
+    }
+}
