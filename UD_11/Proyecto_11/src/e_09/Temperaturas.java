c62da7dab694a9db5e18e415dc5d660c8bf80997
alexCaste02 <alexcaste2002gs@gmail.com>
2023-03-22 16:52:16 +0100

    end 9

--- UD_11/Proyecto_11/src/e_09/Temperaturas.java
+++ UD_11/Proyecto_11/src/e_09/Temperaturas.java
@@ -1,4 +1,42 @@
 package e_09;
 
+
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Temperaturas {
+    public static void main(String[] args) {
+
+
+        //tamaño array segun bisiesto
+        int[] arrayTemps = new int[364];
+
+
+        //generar temps para dias
+        for (int i = 0; i < arrayTemps.length; i++) {
+            arrayTemps[i] = (int) (Math.random() * 21 + 10);
+        }
+
+        //Mapa distribucion de temps
+        //k: temp, v: reps
+        TreeMap<Integer, Integer> distribTemps = new TreeMap<>();
+
+        //contamos repeticiones de temps
+        for (int temp : arrayTemps) {
+            if (distribTemps.containsKey(temp))
+                distribTemps.put(temp, distribTemps.get(temp) + 1);
+            else
+                distribTemps.put(temp, 1);
+        }
+
+        //mostramos temps y reps
+        for (Map.Entry<Integer, Integer> entry : distribTemps.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+        System.out.println("Minima: " + distribTemps.firstKey() + " (" + distribTemps.get(distribTemps.firstKey()) + ")");
+        System.out.println("Maxima: " + distribTemps.lastKey() + " (" + distribTemps.get(distribTemps.lastKey()) + ")");
+
+
+    }
 }
