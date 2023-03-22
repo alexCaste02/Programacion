34fb5999bf280b356a2cfe09f2438ccd9b10c552
alexCaste02 <alexcaste2002gs@gmail.com>
2023-03-22 10:24:19 +0100

    I LOVE STRUCTURE

--- UD_11/Proyecto_11/src/e_08/Main.java
+++ UD_11/Proyecto_11/src/e_08/Main.java
@@ -3,6 +3,7 @@ package e_08;
 import custom.util.InputReader;
 import custom.util.InvalidInputException;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -10,35 +11,42 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Introduce un año");
-        int year=0;
+        int year;
+
         try {
+
             year = InputReader.readAndValidateInt();
+
+            //tamaño array segun bisiesto
+            int[] arrayTemps = (year % 4 == 0 && !(year % 100 == 0) || year % 400 == 0) ?
+                    new int[365] :
+                    new int[366];
+
+            //generar temps para dias
+            for (int i = 0; i < arrayTemps.length; i++) {
+                arrayTemps[i] = (int) (Math.random() * 21 + 10);
+            }
+
+            //Mapa distribucion de temps
+            //k: temp, v: reps
+            Map<Integer, Integer> distribTemps = new HashMap<>();
+
+            //contamos repeticiones de temps
+            for (int temp : arrayTemps) {
+                if (distribTemps.containsKey(temp))
+                    distribTemps.put(temp, distribTemps.get(temp) + 1);
+                else
+                    distribTemps.put(temp, 0);
+            }
+
+            //mostramos temps y reps
+            for (Integer key : distribTemps.keySet()) {
+                System.out.println(key + ": " + distribTemps.get(key));
+            }
+
         } catch (InvalidInputException e) {
             System.out.println(e.getMessage());
         }
-
-        int[] arrayTemps = (year%4==0 && !(year%100==0) || year%400==0)?
-                new int[365]:
-                new int[366];
-
-        for (int i = 0; i < arrayTemps.length; i++) {
-            arrayTemps[i] = (int) (Math.random()*21+10);
-        }
-
-        Map<Integer,Integer> distribTemps = new TreeMap<>();
-
-        for (int temp : arrayTemps) {
-            if(distribTemps.containsKey(temp))
-                distribTemps.put(temp,distribTemps.get(temp)+1);
-            else
-                distribTemps.put(temp,0);
-        }
-
-        for (Integer key : distribTemps.keySet()) {
-            System.out.println(key+": "+distribTemps.get(key));
-        }
-
-
 
     }
 }
