6807ff135fe79666111d0ab014db94bed6b999cf
Alex <alexcaste2002gs@gmail.com>
2023-04-22 14:00:40 +0200

    started exer

---
+++ UD_12/Proyecto_12/src/e_A1/MainA1_UD12.java
@@ -0,0 +1,58 @@
+package e_A1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class MainA1_UD12 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String ruta;
+        boolean repeat = true;
+        do {
+
+            System.out.print("Introduce una ruta del sistema de archivos (No introduzcas nada para finalizar): ");
+            ruta = input.nextLine();
+
+            if(ruta.equals(""))
+                repeat = false;
+            else {
+                try {
+                    muestraInfoRuta(new File(ruta));
+                } catch (FileNotFoundException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+
+        } while (repeat);
+
+    }
+
+    static void muestraInfoRuta(File file) throws FileNotFoundException {
+
+        if(!file.exists()) throw new FileNotFoundException("No existe esa ruta");
+
+
+        if(file.isFile()){
+
+            System.out.println("[A] "+file.getName());
+
+        } else if(file.isDirectory()) {  //Creo que no hace falta, pero asi uso el metodo
+
+            Arrays.stream(Objects.requireNonNull(file.listFiles())).sorted((o1, o2) -> {
+                if (o1.isFile() && o2.isFile() || o1.isDirectory() && o2.isDirectory())
+                    return 0;
+                else if (o1.isFile() && o2.isDirectory())
+                    return 1;
+                else
+                    return -1;
+            }).forEach(f -> System.out.println((f.isFile()?"[A] ":"[*] ")+f.getName()));
+
+
+        }
+
+    }
+}
