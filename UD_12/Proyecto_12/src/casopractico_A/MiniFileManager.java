43285cb93276e267622f966d0f0381da3763fcdf
Alex <alexcaste2002gs@gmail.com>
2023-04-25 20:03:16 +0200

    aprac1

---
+++ UD_12/Proyecto_12/src/casopractico_A/MiniFileManager.java
@@ -0,0 +1,95 @@
+package casopractico_A;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Objects;
+
+public class MiniFileManager {
+
+    private static File carpetaActual = new File("C:/");
+
+    public static String getPathCarpetaActual(){
+        return carpetaActual.getAbsolutePath();
+    }
+
+    public static void cambiarCarpeta(String dir) throws FileNotFoundException {
+        File newfile;
+
+        if(dir.equals("..")) newfile = carpetaActual.getParentFile();
+        else if(dir.matches("C:/.*")) newfile = new File(dir);
+        else newfile = new File(getPathCarpetaActual() + "/" + dir);
+
+        if(!newfile.exists()) throw new FileNotFoundException("No se puede encontrar ruta especificada");
+        carpetaActual = newfile;
+    }
+    
+    public static void listaContenido(){
+        Arrays.stream(Objects.requireNonNull(carpetaActual.listFiles()))
+                .sorted((o1, o2) -> {
+                    if (o1.isDirectory() && o2.isFile()) return -1;
+                    else if (o1.isFile() && o2.isDirectory()) return 1;
+                    return o1.getName().compareTo(o2.getName());
+                }
+        ).forEach(f -> System.out.println((f.isFile()?"[A] ":"[*] ")+f.getName()));
+    }
+
+    public static void listaContenidoInfo(){
+        Arrays.stream(Objects.requireNonNull(carpetaActual.listFiles()))
+                .sorted((o1, o2) -> {
+                            if (o1.isDirectory() && o2.isFile()) return -1;
+                            else if (o1.isFile() && o2.isDirectory()) return 1;
+                            return o1.getName().compareTo(o2.getName());
+                        })
+                .forEach(f -> System.out.printf(
+                            (f.isFile()?"[A] ":"[*] ")
+                            +"%-40s - %8s bytes -      Ultima modificacion: %-15s\n",
+                            f.getName(),f.length(),new Date(f.lastModified())
+                        )
+                );
+    }
+
+    public static boolean crearCarpeta(String dir){
+        File newDir = new File(getPathCarpetaActual()+dir);
+        return newDir.mkdir();
+    }
+
+    public static boolean borrar(File f) throws Exception {
+        if(!f.exists()) throw new FileNotFoundException("La ruta no existe");
+
+        if(f.isFile()) {
+            System.out.println("Borrando archivo "+f.getName());
+        } else {
+            System.out.println("Borrando contenido de "+f.getPath());
+            File[] contenido = f.listFiles();
+            assert contenido != null;
+            int size = contenido.length;
+            int n = 1;
+
+            for (File file : contenido) {
+                if (file.isFile()) {
+                    System.out.printf(
+                            (file.delete() ?
+                                    "Archivo borrado correctamente " :
+                                    "Archivo no ha podido ser borrado ")
+                                    + "[%s] (%d/%d)%n", file.getName(), n, size);
+                } else {
+                    throw new Exception("[AVISO] Esta carpeta contiene subcarpetas. Borrado detenido");
+                }
+                n++;
+            }
+            System.out.println("Borrando directorio...");
+        }
+
+        return f.delete();
+    }
+
+    public static boolean mover(File f1, File f2) throws FileNotFoundException {
+        if(!f1.exists()) throw new FileNotFoundException("La primera ruta no existe");
+        if(!f2.exists()) throw new FileNotFoundException("La segunda ruta no existe");
+
+        return f1.renameTo(f2);
+    }
+}
---
+++ UD_12/Proyecto_12/src/casopractico_A/MiniTerminal.java
@@ -0,0 +1,113 @@
+package casopractico_A;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class MiniTerminal {
+    public static void main(String[] args) {
+
+        boolean repeat = true;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("""
+                 ====  MiniTerminal LinuxEmu  ====
+                 === (c) Alejandro Castellanos ===
+                                  
+                Escribe "help" para obtener los comandos disponibles
+                """
+        );
+        do {
+
+            System.out.print("\n> ");
+            //FIXME: como tratar con espacios en ruta?
+            String enter = input.nextLine();
+
+            String cmd = enter;
+            String ruta1 = "";
+            String ruta2 = "";
+
+
+                String[] line = enter.split(" ");
+                cmd = line[0];
+
+                if(line.length==2)
+                    ruta1 = line[1];
+
+                if(line.length==3) {
+                    ruta2 = line[2];
+                    ruta1 = line[1] + " " + line[2];
+                }
+
+
+            switch (cmd) {
+                case "pwd" -> System.out.println(MiniFileManager.getPathCarpetaActual());
+
+                case "cd" -> {
+                    try {
+                        MiniFileManager.cambiarCarpeta(ruta1);
+                    } catch (FileNotFoundException e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+
+                case "ls" -> MiniFileManager.listaContenido();
+
+                case "ll" -> MiniFileManager.listaContenidoInfo();
+
+                case "mkdir" -> {
+
+                    try {
+                        System.out.println(MiniFileManager.crearCarpeta(ruta1) ?
+                                "Carpeta creada con exito" :
+                                "Carpeta no creada"
+                        );
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+
+                case "rm" -> {
+                    try {
+                        System.out.println(MiniFileManager.borrar(new File(ruta1)) ?
+                                "Borrado con exito" :
+                                "Borrado incompleto"
+                        );
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+
+                case "mv" -> {
+                    try {
+                        System.out.println(MiniFileManager.mover(new File(ruta1), new File(ruta2)) ?
+                                "Borrado con exito" :
+                                "Borrado incompleto"
+                        );
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+
+                case "help" -> System.out.print("""
+                        Lista de comandos:
+                        • pwd:                   Muestra cual es la carpeta actual.
+                        • cd <DIR>:              Cambia la carpeta actual a ‘DIR’. Con .. cambia a la carpeta superior.
+                        • ls:                    Muestra la lista de directorios y archivos de la carpeta actual (primero directorios, luego archivos, ambos ordenados alfabéticamente).
+                        • ll:                    Como ls pero muestra también el tamaño y la fecha de última modificación.
+                        • mkdir <DIR>:           Crea el directorio ‘DIR’ en la carpeta actual.
+                        • rm <FILE>:             Borra ‘FILE’. Si es una carpeta, borrará primero sus archivos y luego la carpeta. Si tiene subcarpetas, las dejará intactas y mostrará un aviso al usuario.
+                        • mv <FILE1> <FILE2>:    Mueve o renombra ‘FILE1’ a ‘FILE2’.
+                        • help:                  Muestra una breve ayuda con los comandos disponibles.
+                        • exit:                  Termina el programa.
+                        """);
+
+                case "exit" -> repeat = false;
+
+                default -> System.out.printf("'%s' no es reconocido como un comando", cmd);
+            }
+
+        } while (repeat);
+    }
+}
