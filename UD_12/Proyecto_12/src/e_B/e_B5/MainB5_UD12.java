5702d6a78a1bc4a51d930315116e17a21fa451a5
alexCaste02 <alexcaste2002gs@gmail.com>
2023-05-02 09:02:52 +0200

    fixes

--- UD_12/Proyecto_12/src/e_B/e_B5/MainB5_UD12.java
+++ UD_12/Proyecto_12/src/e_B/e_B5/MainB5_UD12.java
@@ -10,7 +10,6 @@ letras del abecedario (A.txt, B.txt… Z.txt). Introducirá en cada archivo las 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.io.Writer;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -25,23 +24,24 @@ public class MainB5_UD12 {
                 "Carpeta 'Diccionario' no creada"
         );
 
-        Map<Character,File> mapaLetras = new HashMap<>();
+        Map<String,File> mapaLetras = new HashMap<>();
 
         for (int i = 65; i <= 90; i++) {
-            char letra = (char) i;
+            String letra = String.valueOf((char) i);
             File archivoLetra = new File("UD_12/Proyecto_12/DOCS/Diccionario/"+letra+".txt");
             mapaLetras.put(letra,archivoLetra);
         }
 
 
         // Otro tipo de 'writer' seria mejor?
-        for (Map.Entry<Character, File> entry : mapaLetras.entrySet()) {
+        //TODO: separar vocales para distinguir acentos
+        for (Map.Entry<String, File> entry : mapaLetras.entrySet()) {
             try(PrintWriter pw = new PrintWriter(entry.getValue());
                 Scanner scf = new Scanner(new File("UD_12/Proyecto_12/DOCS/diccionario.txt")))
             {
                 while (scf.hasNext()){
                     String palabra = scf.next();
-                    if(palabra.charAt(0) == entry.getKey()){
+                    if(palabra.toUpperCase().startsWith(entry.getKey())){
                         pw.println(palabra);
                     }
                 }
