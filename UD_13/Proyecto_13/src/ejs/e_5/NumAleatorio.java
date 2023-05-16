24415fb5a79bdf0a60493a8697f8f35f390fc685
Alex <alexcaste2002gs@gmail.com>
2023-05-16 14:50:13 +0200

    .

--- UD_13/Proyecto_13/src/ejs/e_5/NumAleatorio.java
+++ UD_13/Proyecto_13/src/ejs/e_5/NumAleatorio.java
@@ -1,11 +1,7 @@
 package ejs.e_5;
 
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class NumAleatorio extends JFrame{
@@ -16,11 +12,7 @@ public class NumAleatorio extends JFrame{
     private JLabel sliderLabel;
 
     public static void main(String[] args) {
-        start();
-    }
-
-    static void start(){
-        NumAleatorio window = new NumAleatorio("Random - Alex.C.D");
+        new NumAleatorio("Random - Alex.C.D");
     }
 
     public NumAleatorio(String titulo) throws HeadlessException {
@@ -29,17 +21,8 @@ public class NumAleatorio extends JFrame{
         setSize(300,300);
         setLocationRelativeTo(null);
         setContentPane(mainPanel);
-        generarButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                resLabel.setText(ThreadLocalRandom.current().nextInt(maxSlider.getValue())+"");
-            }
-        });
-        maxSlider.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                sliderLabel.setText(maxSlider.getValue()+"");
-            }
-        });
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        generarButton.addActionListener(e -> resLabel.setText(ThreadLocalRandom.current().nextInt(maxSlider.getValue())+""));
+        maxSlider.addChangeListener(e -> sliderLabel.setText(maxSlider.getValue()+""));
     }
 }
